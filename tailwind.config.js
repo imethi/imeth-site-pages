5478797c57ee0a45009ca9f2fd26fe5f41b8db8b
imethi <imethillamperuma@gmail.com>
2025-08-09 18:01:45 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
-  theme: { extend: {} },
+  darkMode: 'class', // Use 'media' for auto based on system preference
+  theme: {
+    extend: {
+      colors: {
+        emerald: {
+          950: '#022c22',
+        },
+      },
+    },
+  },
   plugins: [],
 }
