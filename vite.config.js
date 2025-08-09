f32dd6c825475a5b4804ae94062f547cbef98d13
imethi <imethillamperuma@gmail.com>
2025-08-09 14:00:50 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// IMPORTANT: If your repo name is different, change base to '/<your-repo-name>/'
 export default defineConfig({
-  base: '/imeth-site-pages/',
+  base: '/imeth-site-pages/',   // must match the repo name exactly
   plugins: [react()],
 })
